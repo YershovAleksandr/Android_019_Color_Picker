ab21d7f88360b86e61ceed010c4e9c60310cec7a
hp <namstudionsk@gmail.com>
2018-02-01 23:38:37 +0700

    Fix lv

--- Color_Picker_LV/app/src/main/java/com/nam/color_picker/MainActivity.java
+++ Color_Picker_LV/app/src/main/java/com/nam/color_picker/MainActivity.java
@@ -7,27 +7,39 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    // Remove the below line after defining your own ad unit ID.
-    private static final String TOAST_TEXT = "Test ads are being shown. "
-            + "To show live ads, replace the ad unit ID in res/values/strings.xml with your own ad unit ID.";
 
+    private TextView mRedView;
+    private TextView mGreenView;
+    private TextView mBlueView;
+    private TextView mResultView;
+
+    private SeekBar mRedSeek;
+    private SeekBar mGreenSeek;
+    private SeekBar mBlueSeek;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mRedView = findViewById(R.id.textViewRed);
+        mRedView = findViewById(R.id.textViewGreen);
+        mRedView = findViewById(R.id.textViewBlue);
+
+        mRedSeek = findViewById(R.id.seekBarRed);
+        mGreenSeek = findViewById(R.id.seekBarGreen);
+        mBlueSeek = findViewById(R.id.seekBarBlue);
+
+
         // Load an ad into the AdMob banner view.
         AdView adView = (AdView) findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder()
-                .setRequestAgent("android_studio:ad_template").build();
+        AdRequest adRequest = new AdRequest.Builder().setRequestAgent("android_studio:ad_template").build();
         adView.loadAd(adRequest);
 
-        // Toasts the test ad message on the screen. Remove this after defining your own ad unit ID.
-        Toast.makeText(this, TOAST_TEXT, Toast.LENGTH_LONG).show();
     }
 
 
